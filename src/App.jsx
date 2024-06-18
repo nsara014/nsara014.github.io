fc8859243d2e168554fbe99927996872b6f4a7e9
saranya <nsara014@ucr.edu>
2024-06-18 06:02:38 -0700

    deploy

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import { Contact } from "./components/Contact/Contact";
 import { Experience } from "./components/Experience/Experience";
 import { Hero } from "./components/Hero/Hero";
 import { Navbar } from "./components/Navbar/Navbar";
-import { Projects } from "./components/Projects/Projects";
 
 function App() {
   return (
