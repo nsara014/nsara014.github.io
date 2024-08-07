db92fbd24a0e17d035c9872a9c98d644bb50aeb2
saranya <nsara014@ucr.edu>
2024-08-06 20:53:34 -0700

    vite

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,11 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/nsara014.github.io/",
   plugins: [react()],
   css: {
     modules: {
-      localsConvention: "camelCase",
-    },
-  },
+      localsConvention: "camelCase"
+    }
+  }
 });
